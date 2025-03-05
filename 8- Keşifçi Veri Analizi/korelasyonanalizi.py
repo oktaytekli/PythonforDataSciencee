a75af0f864e0c01d5b5723aa21c0900986944a0e
oktaytekli <oktay.tekli@hotmail.com>
2025-03-05 15:10:18 +0300

    gün 8

--- 8- Keşifçi Veri Analizi/korelasyonanalizi.py
+++ 8- Keşifçi Veri Analizi/korelasyonanalizi.py
@@ -7,3 +7,53 @@ pd.set_option('display.width', 500)
 df = pd.read_csv("data.csv")
 df = df.iloc[:,1:-1]
 df.head()
+
+num_cols = [col for col in df.columns if df[col].dtype in [int,float]]
+corr = df[num_cols].corr()
+
+sns.set(rc={'figure.figsize':(12,12)})
+sns.heatmap(corr,cmap="RdBu")
+plt.show()
+
+### YÜKSEK KORELASYONLU DEĞİŞKENERİN SİLİNMESİ ##
+
+cor_matrix = df.corr().abs()
+
+#        0         1         2         3
+# 0  1.00000  0.117570  0.871754  0.817941
+# 1  0.117570  1.00000  0.428440  0.366126
+# 2  0.871754  0.428440  1.00000  0.962865
+# 3  0.817941  0.366126  0.962865  1.00000
+
+# #  0         1         2         3
+# 0  NaN    0.11757   0.871754   0.817941
+# 1  NaN       NaN    0.428440   0.366126
+# 2  NaN       NaN         NaN   0.962865
+# 3  NaN       NaN         NaN        NaN
+
+upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > 0.90)]
+cor_matrix[drop_list]
+df.drop(drop_list,axis=1)
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr() # korelasyon oluşturduk
+    cor_matrix = corr.abs() #mutlak değerini aldık
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k =1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15,15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+high_correlated_cols(df)
+drop_list = high_correlated_cols(df)
+df.drop(drop_list, axis=1)
+high_correlated_cols(df.drop(drop_list,axis=1),plot=True)
+
+
